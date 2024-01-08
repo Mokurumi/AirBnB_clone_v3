ca90d6f91877811dc9989ce3d127dd8abb622c1e
Alpha Mokurumi <mokurumialpha@gmail.com>
2024-01-09 02:04:58 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -26,10 +26,10 @@ class FileStorage:
 
     def all(self, cls=None):
         """returns the dictionary __objects"""
-        if cls is not None:
+        if cls:
             new_dict = {}
             for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
+                if cls.__name__ == value.__class__.__name__:
                     new_dict[key] = value
             return new_dict
         return self.__objects
